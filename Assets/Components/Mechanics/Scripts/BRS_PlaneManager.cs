f2d57852c0b6f8ca292dcb0c1f5997ea9ac4dd19
AXIUDev <36154929+AXIUDev@users.noreply.github.com>
2018-06-09 10:13:10 +0200

    v0.1 part 9

---
+++ Assets/Components/Mechanics/Scripts/BRS_PlaneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BRS_PlaneManager : MonoBehaviour {
+
+    public GameObject Map;
+    public float Airspeed;
+
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * Time.deltaTime * Airspeed;
+    }
+}
---
+++ Assets/Components/Mechanics/Scripts/PlaneDrop.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDrop : MonoBehaviour
+{
+
+    public bool inPlane;
+    public Camera PlaneCam;
+    public Rigidbody PlayerRig;
+    public GameObject Player1;
+    public bool playerInJumpZone;
+
+    public GameObject PlayerCanvas;
+    public GameObject PlaneCanvas;
+
+    void Start()
+    {
+        inPlane = true;                                                  //Says that the player is in the plane 
+    }
+
+    void OnTriggerStay(Collider other)
+    {                                                                   //Checks if the plane goes into the Jumpzone
+        if (other.gameObject.tag == "JumpZone")
+        {
+            playerInJumpZone = true;
+            //Debug.Log ("In zone"); Optional
+        }
+    }
+
+    void OnTriggerExit(Collider Other)
+    {                                                                    //Checks if the plane goes out of the Jumpzone
+        if (Other.gameObject.tag == "JumpZone")
+        {
+            playerInJumpZone = false;
+            //Debug.Log("Not in zone"); Optional
+        }
+    }
+
+    void Update()
+    {
+
+        if (playerInJumpZone == true)
+        {
+            if (Input.GetKey(KeyCode.F) && inPlane == true)
+            {                                                             //Does the magic of making the player jump out of the plane
+                inPlane = false;
+                PlaneCam.enabled = false;
+                EnablePlayerUI();
+                Player1.SetActive(true);
+                PlayerRig.velocity = Vector3.zero;
+                PlayerRig.angularVelocity = Vector3.zero;
+               
+            }
+        }
+
+        if (inPlane)
+        {
+            Player1.transform.position = transform.position;               //Makes the player follow the plane
+        }
+    }
+
+    void EnablePlayerUI ()
+    {
+        PlayerCanvas.SetActive(true);                                     //Enables the player UI and disables the plane UI 
+        PlaneCanvas.SetActive(false);
+    }
+}
